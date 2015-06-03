2829aaec72b395b47e6fed2dc2b69d5aa9a2927f
Andreas Farre <andreas.farre@gmail.com>
2015-06-03 20:35:14 +0200

    Convert Apply from fundeps to AT

---
+++ src/Control/CCA/Apply.hs
@@ -0,0 +1,17 @@
+{-# Language MultiParamTypeClasses, FlexibleInstances, TypeFamilies #-}
+module Control.CCA.Apply where
+
+class (F f a ~ r, G a r ~ f) => Apply f a r where
+  type F f a
+  type G a r
+  apply:: f -> a -> r
+
+instance Apply f () f where
+  type F f () = f
+  type G () f = f
+  apply = const
+
+instance (Apply y z r, F y z ~ r, G z r ~ y) => Apply (x -> y) (x, z) r where
+  type F (x -> y) (x, z) = F y z
+  type G (x, z) r = x -> G z r
+  apply f (x, z) = apply (f x) z
